bee970520f1eaddcc3632c5a4d71ebd9c5337735
stnuessl <stnuessl@gmail.com>
2015-09-04 15:40:42 +0200

    Fixed some errors when creating links from headlines with special characters.

--- src/main.py
+++ src/main.py
@@ -1,47 +1,66 @@
 #!/usr/bin/python env
 
 import argparse
+import re
 
-#https://github.com/stnuessl/tq#query-top-played-games
+def indentation_level_of(s):
+    
+    cnt = 0
+    
+    for c in s:
+        if c == '#':
+            cnt += 1
+        else:
+            break
+    
+    return cnt
+
+def sanitize_link(link):
+    link = re.sub('[.!?/]', '', link)
+    link = link.replace(' ', '-')
+    
+    return link.lower()
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('README', help='path to the README file')
-    parser.add_argument('-u', '--url', help='URL to the Github respository')
+    parser.add_argument('URL', help='URL to the Github respository')
+    parser.add_argument('-a', '--append', 
+                        help='print the readme after the toc',
+                        action='store_true',
+                        default=False)
+    parser.add_argument('--head', help='', default='# Overview')
     
     args = parser.parse_args()
-
+    
+    if not args.URL.startswith('https://github.com/'):
+        args.URL = 'https://github.com/{}'.format(args.URL)
+    
     readme = open(args.README, mode='r')
     
-    code = False
+    print(args.head)
 
+    code = False
+    
     for line in readme:
-        txt = line.strip(' ')
+        txt = line.strip()
         
         if len(txt) > 2 and txt[:3] == '```':
             code = not code
         elif len(txt) != 0 and txt[0] == '#' and not code:
-            cnt = 0
+            level = indentation_level_of(txt)
+            link = txt[level:].strip()
+            indent = 4 * (level - 1) * ' '
             
-            for c in txt:
-                if c == '#':
-                    cnt += 1
-                else:
-                    break
-
-            txt = txt[cnt:-1].strip(' ')
+            url = '{}#{}'.format(args.URL, sanitize_link(link))
             
-            if len(txt) != 0:
-                intent = cnt * '\t'
-                
-                url = '{}#{}'.format(args.url, txt.replace(' ', '-'))
-                
-                print('{}* [{}]({})'.format(intent, txt, url))
+            print('{}* [{}]({})'.format(indent, link, url))
 
-    readme.seek(0)
-    
-    for line in readme:
-        print(line)
+    if args.append:
+        readme.seek(0)
+        
+        for line in readme:
+            print(line)
 
 if __name__ == '__main__':
     main()
