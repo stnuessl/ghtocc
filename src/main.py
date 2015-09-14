1d77a33482f0965b0db604932f24eb81d6f2431c
stnuessl <stnuessl@gmail.com>
2015-09-14 10:37:18 +0200

    Made --readme optional and if not specified the program tries to directly download the README from the githup repository.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import argparse
 import urllib.request
 import re
 import sys
+import base64
+import json
 
 def indentation_level_of(s):
     return len(s) - len(s.lstrip('#'))
@@ -16,8 +18,8 @@ def to_urlpath(s):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('README', help='path to the README file')
     parser.add_argument('URL', help='URL to the Github respository')
+    parser.add_argument('--readme', help='path to the README file')
     parser.add_argument('-a', '--append', 
                         help='print the readme after the toc',
                         action='store_true',
@@ -34,32 +36,45 @@ def main():
             print('*WARNING*: expected \'https://github.com/\' prefix in \'{}\'- prepending it now'.format(args.URL), file=sys.stderr)
         args.URL = 'https://github.com/{}'.format(args.URL)
     
+    if args.readme == None:
+        repo = args.URL[len('https://github.com/'):]
+        args.readme = 'https://api.github.com/repos/{}/readme'.format(repo)
+    
     if args.debug:
         d = { True : 'On', False : 'Off' }
         
         print('--------------------------------------')
-        print('README : \'{}\''.format(args.README))
+        print('README : \'{}\''.format(args.readme))
         print('URL    : \'{}\''.format(args.URL))
         print('Head   : \'{}\''.format(args.head))
         print('Append : {}'.format(d[args.append]))
         print('Debug  : {}'.format(d[args.debug]))
         print('--------------------------------------')
 
-    path = args.README
+    path = args.readme
     
-    if path.startswith('http') or path.startswith('www'):
+    if path.startswith('http'):
         if args.debug:
-            print('*INFO*: fetching README from remote host')
-            
-        path, _ = urllib.request.urlretrieve(readme)
+            print('*INFO*: fetching README from remote host \'{}\'', path)
+        
+        req = urllib.request.Request(path)
+        req.add_header('Accept', 'application/vnd.github.v3+json')
+        
+        data = urllib.request.urlopen(req).read().decode('utf-8')
 
-    readme = open(path, mode='r')
+        content = json.loads(data)['content']
+        readme = base64.b64decode(content).decode('utf-8')
+    else:
+        readme = open(path, mode='r').read()
 
     print(args.head)
 
     code = False
 
-    for line in readme:
+    for line in readme.splitlines():
+        if len(line) > 0 and line[0] == '\t':
+            continue
+        
         txt = line.strip()
         
         if len(txt) > 2 and txt[:3] == '```':
@@ -74,10 +89,7 @@ def main():
             print('{}* [{}]({})'.format(indent, name, url))
 
     if args.append:
-        readme.seek(0)
-        
-        for line in readme:
-            print(line)
+        print(readme)
 
 if __name__ == '__main__':
     main()
